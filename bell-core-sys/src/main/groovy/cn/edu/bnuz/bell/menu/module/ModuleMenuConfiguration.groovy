5ff45950b7d10404562a63fb2d0cba945c313e29
Yang Lin <todoubaba@gmail.com>
2016-10-09 00:23:34 +0800

    Upgrade to grails 3.2.0 (#27)

--- bell-core-sys/src/main/groovy/cn/edu/bnuz/bell/menu/module/ModuleMenuConfiguration.groovy
+++ bell-core-sys/src/main/groovy/cn/edu/bnuz/bell/menu/module/ModuleMenuConfiguration.groovy
@@ -1,6 +1,5 @@
 package cn.edu.bnuz.bell.menu.module
 
-import cn.edu.bnuz.bell.menu.module.ModuleMenu
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
