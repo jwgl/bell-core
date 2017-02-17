e278e3b61772f2b14341e7a9155206adea3a0803
Yang Lin <todoubaba@gmail.com>
2017-02-17 20:40:27 +0800

    Add open group to place (#52)

--- bell-core-cdm/grails-app/domain/cn/edu/bnuz/bell/place/Place.groovy
+++ bell-core-cdm/grails-app/domain/cn/edu/bnuz/bell/place/Place.groovy
@@ -6,16 +6,64 @@ package cn.edu.bnuz.bell.place
  * @author Yang Lin
  */
 class Place {
+    /**
+     * 场地ID
+     */
     String id
+
+    /**
+     * 场地名称
+     */
     String name
+
+    /**
+     * 英文名称
+     */
     String englishName
+
+    /**
+     * 教学楼
+     */
     String building
+
+    /**
+     * 场地类型
+     */
     String type
+
+    /**
+     * 座位数
+     */
     Integer seat
+
+    /**
+     * 考试座位数
+     */
     Integer testSeat
+
+    /**
+     * 是否可用
+     */
     Boolean enabled
+
+    /**
+     * 是否可用于考试
+     */
     Boolean canTest
+
+    /**
+     * 是否为校外场地
+     */
     Boolean isExternal
+
+    /**
+     * 开门分组
+     */
+    String openGroup
+
+    /**
+     * 备注
+     */
     String note
 
     static hasMany = [
@@ -36,6 +84,7 @@ class Place {
         enabled     comment: '是否可用'
         canTest     comment: '是否可用于考试'
         isExternal  comment: '是否为校外场地'
+        openGroup   comment: '开门分组', length: 50
         note        length: 200, comment: '备注'
     }
 
@@ -43,5 +92,6 @@ class Place {
         seat        nullable: true
         englishName nullable: true, maxSize: 50
         note        nullable: true, maxSize: 200
+        openGroup   nullable: true
     }
 }
