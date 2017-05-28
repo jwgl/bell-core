2b8c588deac0c8ffb652a1e5c7576ce95b09799e
Yang Lin <todoubaba@gmail.com>
2017-05-28 14:47:09 +0800

    Add exam flag to task schedule (#75)

--- bell-core-cdm/grails-app/domain/cn/edu/bnuz/bell/operation/TaskStudent.groovy
+++ bell-core-cdm/grails-app/domain/cn/edu/bnuz/bell/operation/TaskStudent.groovy
@@ -21,7 +21,7 @@ class TaskStudent implements Serializable {
     Date dateCreated
 
     /**
-     * 选课方式-0:排课;1:选课;9:特殊处理
+     * 选课方式-0:排课;1:自选;2:跨专业选课
      */
     Integer registerType
 
@@ -29,6 +29,11 @@ class TaskStudent implements Serializable {
      * 重修类型-0:正常;1:重修;2:及格重修;3:直接考试;9:重考
      */
     Integer repeatType
+
+    /**
+     * 考试标记-0:正常;1:取消资格;2:缓考
+     */
+    Integer examFlag
 
     static belongsTo = [task: Task]
 
@@ -39,8 +44,9 @@ class TaskStudent implements Serializable {
         task         comment: '教学任务'
         student      comment: '学生'
         dateCreated  comment: '选课时间'
-        registerType defaultValue: "0", comment: '选课方式-0:排课;1:选课;9:特殊处理'
+        registerType defaultValue: "0", comment: '选课方式-0:排课;1:自选;2:跨专业选课'
         repeatType   defaultValue: "0", comment: '重修类型-0:正常;1:重修;2:及格重修;3:直接考试;9:重考'
+        examFlag     defaultValue: "0", comment: '考试标记-0:正常;1:取消资格;2:缓考考'
     }
 
     boolean equals(other) {
