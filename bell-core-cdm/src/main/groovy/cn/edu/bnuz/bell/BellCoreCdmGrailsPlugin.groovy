7c9eab24dbe7923138bca95445961f61c911cc50
Yang Lin <todoubaba@gmail.com>
2018-08-26 23:18:03 +0800

    Update to grails 3.1.8 (#98)

--- bell-core-cdm/src/main/groovy/cn/edu/bnuz/bell/BellCoreCdmGrailsPlugin.groovy
+++ bell-core-cdm/src/main/groovy/cn/edu/bnuz/bell/BellCoreCdmGrailsPlugin.groovy
@@ -3,7 +3,7 @@ package cn.edu.bnuz.bell
 import grails.plugins.Plugin
 
 class BellCoreCdmGrailsPlugin extends Plugin {
-    def grailsVersion = "3.2.0 > *"
+    def grailsVersion = "3.3.0 > *"
     def title = "Bell Core Cdm"
     def author = "Yang Lin"
     def authorEmail = "todoubaba@gmail.com"
